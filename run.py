0500fd262c7aaa3ca8aa7466ad7d9ca7fd4b9506
Tanka <tankaroy12@gmail.com>
2025-09-23 20:42:18 +0530

    Url fixed

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from config import PORT, RELOAD
 if __name__ == "__main__":
     run(
         "main:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=PORT,
         reload=RELOAD,
         workers=1
